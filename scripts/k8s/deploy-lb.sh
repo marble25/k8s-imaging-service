a677d9dfbe9414f5e2ba96b2b071cff4947b2452
marble <ljiho1998@naver.com>
2022-01-18 16:17:31 +0900

    link imaging server to traefik

--- scripts/k8s/deploy-lb.sh
+++ scripts/k8s/deploy-lb.sh
@@ -2,6 +2,6 @@ kubectl apply -f ./loadbalancer/ingressroute.yaml
 kubectl apply -f ./loadbalancer/deployment.yaml
 kubectl apply -f ./loadbalancer/service.yaml
 kubectl apply -f ./loadbalancer/traefik.yaml
-kubectl apply -f ./loadbalancer/test.yaml
+# kubectl apply -f ./loadbalancer/test.yaml
 
 kubectl port-forward --address 0.0.0.0 service/traefik 9001:8000 9009:8080 443:4443 -n default
